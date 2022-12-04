cc730a85f785cf59878c230ab433a48c5a536b29
Jessica Smith <jess@mintopia.net>
2022-12-04 16:11:26 +0000

    Day 4

---
+++ src/Day4.php
@@ -0,0 +1,51 @@
+<?php
+namespace Mintopia\Aoc2022;
+
+use Mintopia\Aoc2022\Helpers\Result;
+
+class Day4 extends Day
+{
+
+    protected function loadData(): void
+    {
+        parent::loadData();
+        $this->data = array_map(function(string $line) {
+            return $this->getSections($line);
+        }, $this->data);
+    }
+
+    protected function part1(): Result
+    {
+        $score = 0;
+        foreach ($this->data as [$section1, $section2]) {
+            if (array_diff($section1, $section2) === [] || array_diff($section2, $section1) === []) {
+                $score++;
+            }
+        }
+        return new Result(Result::PART1, $score);
+    }
+
+    protected function part2(Result $part1): Result
+    {
+        $score = 0;
+        foreach ($this->data as [$section1, $section2]) {
+            if (array_intersect($section1, $section2) !== [] || array_intersect($section2, $section1) !== []) {
+                $score++;
+            }
+        }
+        return new Result(Result::PART2, $score);
+    }
+
+    protected function getSections(string $input): array
+    {
+        $sections = explode(',', $input);
+        return array_map(function(string $section) {
+            [$start, $end] = explode('-', $section);
+            $section = [];
+            for ($i = $start; $i <= $end; $i++) {
+                $section[] = $i;
+            }
+            return $section;
+        }, $sections);
+    }
+}
