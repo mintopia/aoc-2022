09204e4f6ad8dbf241e8a2d096b7003765c378e6
Jessica Smith <jessica.smith@fasthosts.com>
2022-12-20 12:01:22 +0000

    Day 20

---
+++ src/Day20.php
@@ -0,0 +1,73 @@
+<?php
+namespace Mintopia\Aoc2022;
+
+use Mintopia\Aoc2022\Helpers\Result;
+
+class Day20 extends Day
+{
+    protected const DECRYPTION_KEY = 811589153;
+
+    protected function loadData(): void
+    {
+        parent::loadData();
+        $this->data = array_map(function(int $index, string $datum): object {
+            return (object) [
+                'value' => (int) $datum,
+                'index' => $index,
+            ];
+        }, array_keys($this->data), $this->data);
+    }
+
+    protected function part1(): Result
+    {
+        $data = $this->data;
+        $answer = $this->getAnswer($data, 1);
+        return new Result(Result::PART1, $answer);
+    }
+
+    protected function part2(Result $part1): Result
+    {
+        $data = array_map(function(object $data): object {
+            $data->value *= self::DECRYPTION_KEY;
+            return $data;
+        }, $this->data);
+        $answer = $this->getAnswer($data, 10);
+        return new Result(Result::PART2, $answer);
+    }
+
+    protected function getAnswer(array $data, int $mix): int
+    {
+        for ($i = 0; $i < $mix; $i++) {
+            $data = $this->mix($data);
+        }
+        $zeroIndex = null;
+        foreach ($data as $zeroIndex => $obj) {
+            if ($obj->value === 0) {
+                break;
+            }
+        }
+        if ($zeroIndex === null) {
+            throw new \Exception('Unable to find index of 0');
+        }
+
+        $count = count($data);
+        $answer = 0;
+        foreach ([1000, 2000, 3000] as $offset) {
+            $answer += $data[($zeroIndex + $offset) % $count]->value;
+        }
+        return $answer;
+    }
+
+    protected function mix(array $input): array
+    {
+        $count = count($input);
+        foreach ($this->data as $original) {
+            $index = array_search($original, $input);
+            $data = $input[$index];
+            $newIndex = ($index + $data->value) % ($count - 1);
+            unset($input[$index]);
+            array_splice($input, $newIndex, 0, [$data]);
+        }
+        return $input;
+    }
+}
